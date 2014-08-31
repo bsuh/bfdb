daf895e6cef1cd1351cbb8241e40b09f1e28eece
Brian Suh <brian.j.suh@gmail.com>
2014-08-31 02:37:19 -0700

    beta version

--- bfdb.js
+++ bfdb.js
@@ -1,59 +1,65 @@
+window.jsel = JSONSelect.match
+
 $(function () {
+  var data;
+
   $('select').chosen();
-
-  $('#groupSelect').change(function () {
-    var selector = $(this).val();
-  });
-
   $.get('info.json', function (r) {
-    data = r;
-
-    var with_name = _.map(data, function (obj, name) {
+    data = _.map(r, function (obj, name) {
       return _.extend({}, obj, {'name': name});
     });
 
-    var by_rarity = _.groupBy(with_name, 'rarity');
-
-    var atk_vs_effhp = _.map(by_rarity, function (list, rarity) {
-      return {
-        key: rarity + '*',
-        values: _.map(list, function (unit) {
-          return {
-            x: unit['lord hp'] + unit['lord def'] / 3,
-            y: unit['lord atk'],
-            size: (unit.sbb && (unit.sbb['max bc generated'] || unit.sbb.levels[9]['max bc generated'])) ||
-              (unit.bb && (unit.bb['max bc generated'] || unit.bb.levels[9]['max bc generated'])) ||
-              unit['max bc generated'],
-            name: unit['name']
-          };
-        })
-      };
+    data = _.filter(data, function (unit) {
+      return (unit.name != 'Metal Mimic');
     });
 
-    var dmg_vs_effhp = _.map(by_rarity, function (list, rarity) {
+    draw();
+  });
+
+  var fnCache = {};
+  function draw() {
+    function query(selector) {
+      return function (unit) {
+        var src = '(unit) -> ' + $(selector).val();
+        var fn = fnCache[src] || eval(CoffeeScript.compile(src, { bare: true }));
+        fnCache[src] = fn;
+        return fn(unit);
+      };
+    };
+
+    if (!data) {
+      return;
+    }
+
+    var groupQuery = query('#groupSelect');
+    var xQuery = query('#xSelect');
+    var yQuery = query('#ySelect');
+    var sizeQuery = query('#sizeSelect');
+
+    var min = groupQuery(_.min(data, groupQuery));
+    var max = groupQuery(_.max(data, groupQuery)) + 1;
+    var tier = (max - min) / 5;
+
+    var grouped = _.groupBy(data, function (unit) {
+      var result = groupQuery(unit);
+      if (typeof result === 'number' && max - min > 10) {
+        return Math.floor(Math.floor((result - min) / tier) * tier + min);
+      } else {
+        return result;
+      }
+    });
+
+    var _data = _.map(grouped, function (list, groupVar) {
       return {
-        key: rarity + '*',
-        values: _.map(list, function (unit) {
-          function getDmg(unit) {
-            var dmg = 'lord damage range';
-            if (unit.sbb && unit.sbb.levels[9][dmg]) {
-              return Number(unit['sbb'].levels[9][dmg].split('~')[1]);
-            }
-
-            if (unit.bb && unit.bb.levels[9][dmg]) {
-              return Number(unit['bb'].levels[9][dmg].split('~')[1]);
-            }
-
-            return Number(unit[dmg].split('~')[1]);
-          }
-
-          return _.extend({}, {
-            x: unit['lord hp'] + unit['lord def'] / 3,
-            y: getDmg(unit),
-            size: (unit.sbb && (unit.sbb['max bc generated'] || unit.sbb.levels[9]['max bc generated'])) ||
-              (unit.bb && (unit.bb['max bc generated'] || unit.bb.levels[9]['max bc generated'])) ||
-              unit['max bc generated'],
-          }, unit);
+        key: groupVar,
+        values: _.filter(_.map(list, function (unit) {
+          return _.extend({
+            x: xQuery(unit),
+            y: yQuery(unit),
+            size: sizeQuery(unit),
+          }, unit)
+        }), function (point) {
+          return isFinite(point.x) && isFinite(point.y);
         })
       };
     });
@@ -65,20 +71,21 @@ $(function () {
         .color(d3.scale.category10().range());
 
       chart.tooltipContent(function(key, x, y, e, graph) {
-        return '<h3>' + e.point.name + '</h3>';
+        return '<h3>' + e.point.name + '</h3><p>' +
+          $('#groupSelect option:selected').text() + ': ' + groupQuery(e.point) +
+          '<br/>' +
+          $('#sizeSelect option:selected').text() + ': ' + e.point.size + '</p>';
       });
 
-      chart.xAxis.tickFormat(d3.format('100f'));
-      chart.yAxis.tickFormat(d3.format('100f'));
-
       d3.select('#chart svg')
-        .datum(dmg_vs_effhp)
-        .transition().duration(500)
+        .datum(_data)
         .call(chart);
 
       nv.utils.windowResize(chart.update);
 
       return chart;
     });
-  });
+  }
+
+  $('select').change(draw);
 });
